ff04510fba895b8e67c139be500e64564e7bdf56
aerenchyma <jczetta@gmail.com>
2013-05-08 22:53:38 -0400

    picking out elements

--- parser_take1.py
+++ parser_take1.py
@@ -8,9 +8,23 @@ from bs4 import BeautifulSoup
 test_recipe = "http://allrecipes.com/Recipe/Mandarin-Chicken-Pasta-Salad/"
 rec_doc = BeautifulSoup(urllib2.urlopen(test_recipe))
 
-print rec_doc
+title = rec_doc.title.string.strip().replace(" - Allrecipes.com","")
+
+srv_num = rec_doc.find(id="lblYield").string
+
+# for each p class=fl-ing
+	# class ingredient-amount + class ingredient-name
+	# (both of these are with in the p)
+
+test_ingr = rec_doc.find("p", {"class" : "fl-ing"}) # this is only finding the first ingredient, select for others
 
 
+
+#### testing
+
+print title
+print srv_num
+print [str(x.string.strip()) for x in test_ingr if x != '']
 
 
 
