de5257c5a737697301bf3a169a23caec2573ccdd
aerenchyma <jczetta@gmail.com>
2013-05-08 22:33:17 -0400

    yeah forget lxml without beautifulsoup

--- parser_take1.py
+++ parser_take1.py
@@ -1,8 +1,15 @@
 # recipe parser, Take 2
 
 import lxml.html as lh
+import urllib2
+from bs4 import BeautifulSoup
+
 
 test_recipe = "http://allrecipes.com/Recipe/Mandarin-Chicken-Pasta-Salad/"
+rec_doc = BeautifulSoup(urllib2.urlopen(test_recipe))
+
+print rec_doc
+
 
 
 
