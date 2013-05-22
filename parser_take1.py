8a79f282fec5861196a4f00695c1f5dee39d693e
aerenchyma <jczetta@gmail.com>
2013-05-21 22:56:17 -0400

    few comments and cleanup, space to take away

--- parser_take1.py
+++ parser_take1.py
@@ -16,7 +16,7 @@ import re
 def word_sans_comma(w):
 	if w[-1] =="," or w[-1] == ".":
 		#print "comma repl", w
-		w = w.replace(",","") # because replacing is destructive and need a non-destructive fxn
+		w = w.replace(",","") # because replacing is destructive and need non-destructive fxn
 		w = w.replace(".","")
 	return w
 
@@ -26,9 +26,6 @@ rec_doc = BeautifulSoup(urllib2.urlopen(test_recipe))
 ingreds_dict = {}
 
 stopwords = ["in", "on", "the", "of", "what", "and", "&", "are", "you", "a", "an", "or", "why"] # extend / grab from elsewhere??
-cooking_verbs = ["browned", "softened"] # add more if not get from elsewhere/learning
-
-
 
 title = rec_doc.title.string.strip().replace(" - Allrecipes.com","")
 srv_num = rec_doc.find(id="lblYield").string
@@ -56,28 +53,30 @@ alldirs = " ".join([x.string for x in directions.findAll("span", "plaincharacter
 
 place = 0
 
-print ingreds_dict
+#print ingreds_dict
 
-debugging = []
 replaced = []
+
 for w in [x.encode('utf-8') for x in alldirs.split() if x != "" and x != " " and "ed" not in x[-3:]]:
 	print "w is:", w
 	#print "adding ed: ", w + "ed" # never get brown here, currently
 	for ig_wlst in [y.split() for y in ingreds_dict]:
 		#print ig_wlst
+		sentend = False
 		ig_wlst = [x.encode('utf-8') for x in ig_wlst]
 		if (word_sans_comma(w) in ig_wlst or w in ig_wlst) and word_sans_comma(w.encode('utf-8')) not in [x.encode('utf-8') for x in replaced]: #and w.encode('utf-8') not in " ".join(ig_wlst): # encoding consistent enough here?
 			#print "index",ig_wlst.index(w), w
 			#if not 
 			#alldirs = alldirs.replace(w, ingreds_dict[" ".join(ig_wlst)] + " " + " ".join(ig_wlst)) # replaces ALL instances of str
-			print ingreds_dict[" ".join(ig_wlst)] + " " + " ".join(ig_wlst)
-			alldirs = re.sub(re.escape(w) + r"[^a-zA-Z]", " " + ingreds_dict[" ".join(ig_wlst)] + " " + " ".join(ig_wlst) + "  ", alldirs) # note the two spaces at the end to allow for easy dedupe of words
+			#print ingreds_dict[" ".join(ig_wlst)] + " " + " ".join(ig_wlst)
+			subst_str = ingreds_dict[" ".join(ig_wlst)] + " " + " ".join(ig_wlst) + " " # note spacing changes -- TODO make neater/clearer what's going on
+			alldirs = re.sub(re.escape(w) + r"[^a-zA-Z]", " " + " " + subst_str + " " if w[-1] != "." else subst_str + ". ", alldirs) 
 			#print "End of ALLDIRS: ", alldirs[-20:]
 			place = alldirs.find(w,place) + len(" ".join(ig_wlst))# plus some amount...?? or not
 			
 			replaced += [x.encode('utf-8') for x in ig_wlst] #hmm
 
-print replaced
+#print replaced
 
 
 ct = 0
